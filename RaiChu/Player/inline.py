18b93d3b663bcca02bd33890431358980ef61eb3
Hell-x <106371589+Hell-x@users.noreply.github.com>
2022-05-30 03:38:57 +0530

    Update inline.py

--- RaiChu/Player/inline.py
+++ RaiChu/Player/inline.py
@@ -28,37 +28,37 @@ async def inline(client: Client, query: InlineQuery):
         InlineQueryResultArticle(
             title="Pause Stream",
             description=f"Pause the current playout on group call.",
-            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
+            thumb_url="https://telegra.ph/file/f53f4c190e8c3d0af5fec.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Resume Stream",
             description=f"Resume the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
+            thumb_url="https://telegra.ph/file/f53f4c190e8c3d0af5fec.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Mute Stream",
             description=f"Mute the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
+            thumb_url="https://telegra.ph/file/f53f4c190e8c3d0af5fec.jpg",
             input_message_content=InputTextMessageContent("/mute"),
         ),
         InlineQueryResultArticle(
             title="Unmute Stream",
             description=f"Unmute the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
+            thumb_url="https://telegra.ph/file/f53f4c190e8c3d0af5fec.jpg",
             input_message_content=InputTextMessageContent("/unmute"),
         ),
         InlineQueryResultArticle(
             title="Skip Stream",
             description=f"Skip to next track. | For Specific track number: /skip [number] ",
-            thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
+            thumb_url="https://telegra.ph/file/f53f4c190e8c3d0af5fec.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
             title="End Stream",
             description="Stop the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
+            thumb_url="https://telegra.ph/file/f53f4c190e8c3d0af5fec.jpg",
             input_message_content=InputTextMessageContent("/stop"),
         ),
       ]
