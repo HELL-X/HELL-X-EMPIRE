632e32be3b984f9a649e40220f17a72ada18cd66
Hell-x <106371589+Hell-x@users.noreply.github.com>
2022-05-30 03:47:39 +0530

    Update video.py

--- RaiChu/Player/video.py
+++ RaiChu/Player/video.py
@@ -21,7 +21,7 @@ from pytgcalls.types.input_stream.quality import (
 from youtubesearchpython import VideosSearch
 from Process.design.thumbnail import play_thumb, queue_thumb
 
-IMAGE_THUMBNAIL = "https://telegra.ph/file/519b6bc739756cb822039.png"
+IMAGE_THUMBNAIL = "https://telegra.ph/file/f53f4c190e8c3d0af5fec.jpg"
 
 def ytsearch(query: str):
     try:
@@ -183,8 +183,8 @@ async def vplay(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/BotDuniyaXd"),
-                            InlineKeyboardButton("💭 Support", url=f"https://t.me/godzilla_chatting")
+                            InlineKeyboardButton("📣 Channel", url=f"https://MAFIA_EBD"),
+                            InlineKeyboardButton("💭 Support", url=f"https://t.me/EBD_WORLD")
                         ],
                         [
                             InlineKeyboardButton("🗑 Close", callback_data="cls")
@@ -265,8 +265,8 @@ async def vplay(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/BotDuniyaXd"),
-                            InlineKeyboardButton("💭 Support", url=f"https://t.me/godzilla_chatting")
+                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/MAFIA_EBD"),
+                            InlineKeyboardButton("💭 Support", url=f"https://t.me/EBD_WORLD")
                         ],
                         [
                             InlineKeyboardButton("🗑 Close", callback_data="cls")
